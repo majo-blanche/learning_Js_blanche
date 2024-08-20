e243cbfbcd910085e909820a15e84d1d83705d05
majo-blanche <blanchemajo24@gmail.com>
2024-08-20 11:05:11 +0100

    function

--- day-2/funtions.js
+++ day-2/funtions.js
@@ -10,3 +10,12 @@ function add(x,y,z){
 let answer=add(2,5,9)
 console.log(answer)
 //converting from fahrenheit to celsius
+function tocelsius(fahrenheit){
+    return(5/9)*(fahrenheit-32)
+}
+let value="the temperature is"+tocelsius(55)+"celsius";
+console.log(value)
+//funtion expression
+const x=funtion (a,b){return (a*b)};
+let z=x(4,3)
+console.log{z}
