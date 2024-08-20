34ce9ed9736d14ce3e0abeb7b0bd42ed55ded0da
majo-blanche <blanchemajo24@gmail.com>
2024-08-20 09:58:18 +0100

    funtions

---
+++ day-2/funtions.js
@@ -0,0 +1,12 @@
+function product(p1,p2){
+    return(p1*p2)
+}
+let result=product(5,4)
+console.log(result)
+//adding numbers
+function add(x,y,z){
+    return(x+y+z)
+}
+let answer=add(2,5,9)
+console.log(answer)
+//converting from fahrenheit 
