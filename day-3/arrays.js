d62c52b127746f4c28e3a4c618ba266f18802778
majo-blanche <blanchemajo24@gmail.com>
2024-08-21 12:34:43 +0100

    Arrays

--- day-3/arrays.js
+++ day-3/arrays.js
@@ -23,4 +23,29 @@ for (const key of iterator) {
 const newColors = colors.toReversed();
 console.log(colors.toReversed())
 
-//different ways of creating an array
+/*different ways of creating an array
+// 'fruits' array created using array literal notation.
+const fruits1 = ["Apple", "Banana"];
+console.log(fruit1.length);
+// 'fruits2' array created using the Array() constructor.
+const fruits2 = new Array("Apple", "Banana");
+console.log(fruits2.length);
+// 'fruits3' array created using String.prototype.split().
+const fruits3 = "Apple, Banana".split(", ");
+console.log(fruits3.length);
+*/
+//creating a string from an array
+const animals=["pig","dog"]
+    const animalsString=animals.join(",");
+console.log(animalsString);
+//access an array item by its index
+const shapes=["square","triangle","regtangle","cicle"]
+shapes[0];
+console.log(shapes[0]);
+//checking if an arr contain aparticular element
+console.log(shapes.includes("banana"));
+console.log(shapes.indexOf("triangle"));
+//append an item to an array
+const newlength=shapes.push("trapezium");
+console.log(shapes);
+console.log(newlength);
