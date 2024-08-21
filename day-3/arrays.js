fd90a94413930806caf908fadc348d6be789ac90
majo-blanche <blanchemajo24@gmail.com>
2024-08-21 10:20:59 +0100

    Arrays

--- day-3/arrays.js
+++ day-3/arrays.js
@@ -1,3 +1,13 @@
 const fruits=[];
 fruits.push("banana","apple","peach");
 console.log(fruits.length);
+fruits[5]="mango";
+console.log(fruits[5]);
+console.log(Object.keys(fruits));
+console.log(fruits.length)
+
+const color=["red","yellow","blue"];
+color[5]="purple";
+color.forEach((item,index))= {
+
+}
