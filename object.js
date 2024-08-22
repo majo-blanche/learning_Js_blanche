f613c274b42a4d60f8e6193ceca57a1773fa362a
majo-blanche <blanchemajo24@gmail.com>
2024-08-22 11:54:02 +0100

    Arrays

--- object.js
+++ object.js
@@ -6,10 +6,16 @@ const girl={
     nationality:"english",
     fullname:function(){
         return this.firstname+" "+this.lastname;
-        
-    }
-    
-};
+         },
+         
+        //nested object
+ cars :{
+    car1:"ford",
+    car2:"bmw",
+    car3:"fiat"
+}
+    };
+delete girl.age;//deleting
 console.log(girl.firstname +" is " +girl.age +" year old");
 
 console.log(girl);
@@ -20,4 +26,10 @@ person.firstname="majo";
 person.lastname="blanche";
 person.age=50;
 person.eyecolor="black"
+delete person.age;
+person.name= function(){
+    return(this.firstname+" "+this.lastname).toUpperCase();
+};
 console.log(person)
+console.log(person.name());
+
