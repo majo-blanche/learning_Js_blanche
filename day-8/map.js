b0f46658a118cecb6646908412e905b99838e31c
majo-blanche <blanchemajo24@gmail.com>
2024-08-28 13:19:20 +0100

    MAP

--- day-8/map.js
+++ day-8/map.js
@@ -2,7 +2,7 @@ let map = new Map();
 map.set('1','goodbye');
 map.set(1,'hello');
 map.set(true,'false');
-let obt=Object.fromEntries(map.entries());
+let obt=Object.fromEntries(map);
 console.log(obt.true);
 console.log(map.get(1));
 console.log(map.get('1'));
@@ -14,12 +14,13 @@ let visitCountMap=new Map();
 visitCountMap.set(john,123);
 console.log(visitCountMap.get(john));
 
- //iteratingo over map
+ //iterating over map
 let recipeMap=new Map([
     ['orange',500],
      ['tomato',350],
      ['onion',50]
  ]);
+ 
 for(let vegetable of recipeMap.keys()){
      console.log(vegetable);
  }
@@ -49,3 +50,26 @@ let price=Object.fromEntries([
     ['fish',6],
 ]);
 console.log(price.fish);
+
+//set
+let set=new Set();
+let johne={ name:"john"};
+let peter={ name:"peter"};
+let mary={name:"mary"};
+set.add(john);
+set.add(peter);
+set.add(mary);
+set.add(john);
+set.add(peter);
+console.log(set.size);
+for(let user of set){
+    console.log(user.name)
+}
+//iteration over set
+let sit=new Set(["majo","nekam","blanche"]);
+for(let value of sit){
+    console.log(value);
+}
+sit.forEach((value, sit) => {
+    console.log(value);
+  });
